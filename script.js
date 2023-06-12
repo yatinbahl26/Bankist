c8e78d14d5d6350b110f18071bca2033fe5d8d48
yatinbahl26 <bahl.yatin26@gmail.com>
2023-06-12 23:49:58 +0530

    Adding index.html

---
+++ script.js
@@ -0,0 +1,215 @@
+'use strict';
+
+// Data
+const account1 = {
+  owner: 'Jonas Schmedtmann',
+  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+  interestRate: 1.2, // %
+  pin: 1111,
+};
+
+const account2 = {
+  owner: 'Jagmeet Kaur',
+  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+  interestRate: 1.5,
+  pin: 2222,
+};
+
+const account3 = {
+  owner: 'Steven Thomas Williams',
+  movements: [200, -200, 340, -300, -20, 50, 400, -460],
+  interestRate: 0.7,
+  pin: 3333,
+};
+
+const account4 = {
+  owner: 'Sarah Smith',
+  movements: [430, 1000, 700, 50, 90],
+  interestRate: 1,
+  pin: 4444,
+};
+
+const accounts = [account1, account2, account3, account4];
+
+// Elements
+const labelWelcome = document.querySelector('.welcome__message');
+const labelSumIn = document.querySelector('.summary__value--in');
+const labelSumOut = document.querySelector('.summary__value--out');
+const labelSumInterest = document.querySelector('.summary__value--interest');
+const labelBalance = document.querySelector('.account__balance');
+
+let containerApp = document.querySelector('.app');
+let containerMovements = document.querySelector('.movements__section');
+
+const btnLogin = document.querySelector('.form__btn--login');
+const btnTransfer = document.querySelector('.form__btn--transfer');
+const btnLoan = document.querySelector('.form__btn--loan');
+const btnClose = document.querySelector('.form__btn--close');
+const btnSort = document.querySelector('.btn--sort');
+
+const inputLoginUsername = document.querySelector('.input--form--user');
+const inputLoginPin = document.querySelector('.input--form--PIN');
+const inputTransferTo = document.querySelector('.form__input--to');
+const inputTransferAmount = document.querySelector('.form__input--amount');
+const inputLoanAmount = document.querySelector('.form__input--loan');
+const inputCloseUsername = document.querySelector('.form__input--user');
+const inputClosePin = document.querySelector('.form__input--pin');
+
+const displayMovements = (movements, sorted = false) => {
+  const movs = sorted ? movements.slice().sort((a, b) => a - b) : movements;
+  containerMovements.innerHTML = '';
+  movs.forEach((movement, i) => {
+    const type = movement > 0 ? 'deposit' : 'withdrawal';
+
+    const html = `
+    <div class="movements__row">
+        <div class="movements__type movements__type--${type}">${
+      i + 1
+    } ${type}</div>
+        <div class="movements__value">${movement}€</div>
+  </div>
+    `;
+    containerMovements.insertAdjacentHTML('afterbegin', html);
+  });
+};
+
+const createUserNames = accs => {
+  accs.forEach(acc => {
+    acc.userName = acc.owner
+      .toLowerCase()
+      .split(' ')
+      .map(word => word[0])
+      .join('');
+  });
+};
+
+createUserNames(accounts);
+
+const calcDisplayBalance = acc => {
+  acc.balance = acc.movements.reduce((acc, curr) => acc + curr, 0);
+  labelBalance.textContent = `${acc.balance}€`;
+};
+
+const calcDisplaySummary = function (acc) {
+  console.log(acc);
+  const incomes = acc.movements
+    .filter(mov => mov > 0)
+    .reduce((acc, mov) => acc + mov);
+
+  const out = acc.movements
+    .filter(mov => mov < 0)
+    .reduce((acc, mov) => acc + mov);
+
+  const interests = acc.movements
+    .filter(mov => mov > 0)
+    .map(deposit => (deposit * acc.interestRate) / 100)
+    .filter(int => {
+      return Math.round(int >= 1);
+    })
+    .reduce((acc, int) => acc + int);
+
+  labelSumIn.textContent = `${incomes}€`;
+  labelSumOut.textContent = `${Math.abs(out)}€`;
+  labelSumInterest.textContent = `${interests}€`;
+};
+
+let UpdateUI = acc => {
+  // Display ACCOUNT'S Balance
+  calcDisplayBalance(acc);
+
+  calcDisplaySummary(acc);
+
+  displayMovements(acc.movements);
+};
+
+let currentAccount;
+
+btnLogin.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  currentAccount = accounts.find(
+    acc => acc.userName === inputLoginUsername.value
+  );
+
+  console.log(currentAccount);
+
+  if (currentAccount?.pin === Number(inputLoginPin.value)) {
+    containerApp.style.opacity = 100;
+    labelWelcome.textContent = `Welcome back, ${
+      currentAccount.owner.split(' ')[0]
+    }`;
+
+    UpdateUI(currentAccount);
+
+    // clear input fields
+    inputLoginUsername.value = inputLoginPin.value = '';
+    containerApp.blur();
+  } else {
+    alert('Please enter correct credentials');
+  }
+});
+
+btnTransfer.addEventListener('click', function (e) {
+  e.preventDefault();
+  const amount = Number(inputTransferAmount.value);
+  const receiverAcc = accounts.find(acc => {
+    return acc.userName === inputTransferTo.value;
+  });
+  console.log(receiverAcc);
+
+  if (
+    amount > 0 &&
+    receiverAcc &&
+    receiverAcc.userName !== currentAccount.userName &&
+    currentAccount?.balance >= amount
+  ) {
+    receiverAcc.movements.push(amount);
+    currentAccount.movements.push(-amount);
+
+    inputTransferTo.value = inputTransferAmount.value = '';
+    UpdateUI(currentAccount);
+  } else {
+    alert("Please enter correct receiver's account details to transfer");
+  }
+});
+
+btnLoan.addEventListener('click', function (e) {
+  e.preventDefault();
+  const loanAmount = Number(inputLoanAmount.value);
+  if (
+    loanAmount > 0 &&
+    currentAccount.movements.some(mov => mov >= loanAmount * 0.1)
+  ) {
+    currentAccount.movements.push(loanAmount);
+    inputLoanAmount.value = '';
+    containerApp.style.opacity = 100;
+    UpdateUI(currentAccount);
+  }
+});
+
+btnClose.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  if (
+    inputCloseUsername.value === currentAccount.userName &&
+    currentAccount.pin === Number(inputClosePin.value)
+  ) {
+    const closeAccount = accounts.findIndex(acc => {
+      return acc.userName === inputCloseUsername.value;
+    });
+    containerApp.style.opacity = 0;
+
+    accounts.splice(closeAccount, 1);
+    inputCloseUsername.value = inputClosePin.value = '';
+  } else {
+    alert('Enter correct account details to close your account');
+  }
+});
+
+let sorted = false;
+
+btnSort.addEventListener('click', function (e) {
+  e.preventDefault();
+  displayMovements(currentAccount.movements, !sorted);
+  sorted = !sorted;
+});
